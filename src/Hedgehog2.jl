d492a659a8236f891445e2f9bce5b3cba44d1462
aleCombi <alecombi94@gmail.com>
2025-03-11 20:29:40 +0100

    btime back to main scope, other wwise it doesnt work

--- src/Hedgehog2.jl
+++ src/Hedgehog2.jl
@@ -8,31 +8,27 @@ include("pricing_methods.jl")
 include("delta_methods.jl")
 
 """Example code with benchmarks"""
-function example()
-    # Define market data and payoff
-    market_inputs = BlackScholesInputs(0.01, 1, 0.4)
-    payoff = VanillaEuropeanCall(1, 1)
-    pricer = Pricer(market_inputs, payoff, BlackScholesMethod())
-    println(pricer())
-    # Analytical Delta
-    analytical_delta_calc = DeltaCalculator(pricer, BlackScholesAnalyticalDelta())
-    println(analytical_delta_calc())
+# Define market data and payoff
+market_inputs = BlackScholesInputs(0.01, 1, 0.4)
+payoff = VanillaEuropeanCall(1, 1)
+pricer = Pricer(market_inputs, payoff, BlackScholesMethod())
+println(pricer())
+# Analytical Delta
+analytical_delta_calc = DeltaCalculator(pricer, BlackScholesAnalyticalDelta())
+println(analytical_delta_calc())
 
-    # AD Delta
-    ad_delta_calc = DeltaCalculator(pricer, ADDelta())
-    println(ad_delta_calc())
+# AD Delta
+ad_delta_calc = DeltaCalculator(pricer, ADDelta())
+println(ad_delta_calc())
 
-    println("Benchmarking pricer:")
-    @btime pricer()
+# println("Benchmarking pricer:")
+@btime pricer()
 
-    # Run benchmarks
-    println("Benchmarking Analytical Delta:")
-    @btime analytical_delta_calc()
+# Run benchmarks
+println("Benchmarking Analytical Delta:")
+@btime analytical_delta_calc()
 
-    println("Benchmarking AD Delta:")
-    @btime ad_delta_calc()
-end
-
-example() 
+println("Benchmarking AD Delta:")
+@btime ad_delta_calc()
 
 end
