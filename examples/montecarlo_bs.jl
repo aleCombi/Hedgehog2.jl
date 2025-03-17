ce10015c13058ab28fc4c1e5373f99585a469828
aleCombi <alecombi94@gmail.com>
2025-03-17 18:43:35 +0100

    Added black scholes montecarlo

---
+++ examples/montecarlo_bs.jl
@@ -0,0 +1,28 @@
+using Revise, Hedgehog2, BenchmarkTools, Dates
+
+"""Example code with benchmarks"""
+
+# Define market inputs
+reference_date = Date(2020, 1, 1)
+rate=0
+spot=1
+sigma=0.4
+market_inputs = BlackScholesInputs(reference_date, rate, spot, sigma)
+
+# Define payoff
+expiry = Date(2021, 1, 1)
+strike = 1.2
+payoff = VanillaOption(strike, expiry, Hedgehog2.European(), Hedgehog2.Call(), Hedgehog2.Spot())
+
+# Define montecarlo methods
+trajectories = 100000000
+method = Hedgehog2.BSMontecarlo(trajectories)
+
+# Define pricer
+mc_pricer = Pricer(payoff, market_inputs, method)
+
+# Define analytical pricer
+analytical_pricer = Pricer(payoff, market_inputs, BlackScholesMethod())
+
+println(analytical_pricer())
+println(mc_pricer())
--- src/pricing_methods/montecarlo.jl
+++ src/pricing_methods/montecarlo.jl
@@ -8,11 +8,19 @@ end
 
 # we could make an ExactMontecarlo, dispath to get the noise problem and always make just one step.
 function compute_price(payoff::VanillaOption{European, C, Spot}, market_inputs::BlackScholesInputs, method::BSMontecarlo) where {C}
-    T = Dates.value.(payoff.expiry .- marketInputs.referenceDate) ./ 365  # Assuming 365-day convention
-    noise = GeometricBrownianMotionProcess(market_inputs.r, market_inputs.sigma, market_inputs.referenceDate, 0)
+    T = Dates.value.(payoff.expiry .- market_inputs.referenceDate) ./ 365  # Assuming 365-day convention
+    
+    noise = GeometricBrownianMotionProcess(market_inputs.rate, market_inputs.sigma, 0.0, 1.0)
     problem = NoiseProblem(noise, (0, T))
-    ensemble_problem = EnsembleProblem(problem)
-    solution = solve(ensemble_problem; dt=T, trajectories = method.trajectories) # its an exact simulation, hence we use just one step
-    mean_payoff = mean(payoff.(last.(solution.u)))
-    return mean_payoff
+    solution = solve(EnsembleProblem(problem); dt=T, trajectories = method.trajectories) # its an exact simulation, hence we use just one step
+    
+    spot = market_inputs.forward * exp(- market_inputs.rate * T)
+    antithetic_noise = GeometricBrownianMotionProcess(market_inputs.rate, -market_inputs.sigma, 0.0, spot)
+    antithetic_problem = NoiseProblem(antithetic_noise, (0, T), seed=problem.seed)
+    antithetic_solution = solve(EnsembleProblem(antithetic_problem); dt=T, trajectories = method.trajectories, seed=problem.seed) # its an exact simulation, hence we use just one step
+    
+    final_payoffs = (payoff.(last.(solution.u)) + payoff.(last.(antithetic_solution.u))) * 0.5
+    mean_payoff = mean(final_payoffs)
+    var_payoff = var(final_payoffs)
+    return mean_payoff, var_payoff
 end
