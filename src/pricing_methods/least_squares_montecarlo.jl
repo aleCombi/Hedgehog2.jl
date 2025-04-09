328038b7b5ba44c4a425232b45fc1731a184db5a
Combi, A (Alessandro) <alessandro.combi@rabobank.com>
2025-04-09 16:55:10 +0200

    Removed type instability and added support for antithetic variates in lsm

--- src/pricing_methods/least_squares_montecarlo.jl
+++ src/pricing_methods/least_squares_montecarlo.jl
@@ -9,8 +9,8 @@ Uses regression to estimate continuation values and determine optimal stopping t
 - `mc_method`: A `MonteCarlo` method specifying dynamics and simulation strategy.
 - `degree`: Degree of the polynomial basis for regression.
 """
-struct LSM <: AbstractPricingMethod
-    mc_method::MonteCarlo
+struct LSM{M<:MonteCarlo} <: AbstractPricingMethod
+    mc_method::M
     degree::Int  # degree of polynomial basis
 end
 
@@ -50,16 +50,32 @@ function extract_spot_grid(sol::EnsembleSolution)
     return spot_grid
 end
 
+function extract_spot_grid(sol_anti::Tuple{EnsembleSolution,EnsembleSolution})
+    sol, antithetic = sol_anti
+    npaths = length(sol.u)
+    nsteps = length(sol.u[1].t)
+    spot_grid = Matrix{eltype(sol.u[1].u[1][1])}(undef, nsteps, 2*npaths)
+
+    @inbounds for j in 1:npaths
+        @views spot_grid[:, j] = getindex.(sol.u[j].u, 1)
+    end
+
+    @inbounds for k in (npaths+1):(2*npaths)
+        @views spot_grid[:, k] = getindex.(antithetic.u[k-npaths].u, 1)
+    end
+
+    return spot_grid
+end
+
 
 function solve(
-    prob::PricingProblem{VanillaOption{TS,TE,American,C,Spot},I},
-    method::LSM,
-) where {TS,TE,I<:AbstractMarketInputs,C}
+    prob::PricingProblem{VanillaOption{TS,TE,American,C,S},I},
+    method::L,
+) where {TS,TE,I<:AbstractMarketInputs,C,S, L<:LSM}
 
     T = yearfrac(prob.market_inputs.referenceDate, prob.payoff.expiry)
-    sol = simulate_paths(prob, method.mc_method, NoVarianceReduction())
-    spot_grid = extract_spot_grid(sol) ./ prob.market_inputs.spot  # Normalize paths
-
+    sol = Hedgehog2.simulate_paths(prob, method.mc_method, method.mc_method.config.variance_reduction)
+    spot_grid = Hedgehog2.extract_spot_grid(sol) ./ prob.market_inputs.spot  # Normalize paths
     ntimes, npaths = size(spot_grid)
     nsteps = ntimes - 1
     discount = df(prob.market_inputs.rate, add_yearfrac(prob.market_inputs.referenceDate, T / nsteps))
@@ -67,7 +83,7 @@ function solve(
     # (time_index, value) for each path
     stopping_info = [(nsteps, prob.payoff(spot_grid[nsteps+1, p])) for p = 1:npaths]
 
-    for i = (ntimes-1):-1:2
+    for i = nsteps:-1:2
         t = i - 1
 
         continuation =
@@ -80,7 +96,7 @@ function solve(
         x = spot_grid[i, in_the_money]
         y = continuation[in_the_money]
         poly = Polynomials.fit(x, y, method.degree)
-        cont_value = poly.(x)
+        cont_value = map(poly, x)
 
         update_stopping_info!(stopping_info, in_the_money, cont_value, payoff_t, t)
     end
