ceb88366292e75835ebe53c7d7bebdad9749d93d
aleCombi <alecombi94@gmail.com>
2025-04-12 11:44:03 +0200

    Propert dates unit testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,3 @@
 using Revise, Hedgehog2, Test, Dates, Distributions
 
-include("unit/payoff.jl")
-include("unit/vol_surface.jl")
 include("unit/date_functions.jl")
-include("black_scholes.jl")
-include("binomial_tree.jl")
-include("carr_madan.jl")
-include("least_squares_montecarlo.jl")
-include("implied_vol.jl")
-include("rate_curve.jl")
-include("greeks.jl")
-include("montecarlo_black_scholes.jl")
-include("montecarlo_heston.jl")
-include("antithetic_variates.jl")
