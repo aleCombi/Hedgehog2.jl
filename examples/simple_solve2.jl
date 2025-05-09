1658c8b631b04f627bc3e390cfd0c9427ee00ab5
aleCombi <alecombi94@gmail.com>
2025-05-09 15:53:06 +0200

    First quick solve with alloc

--- examples/simple_solve2.jl
+++ examples/simple_solve2.jl
@@ -1,4 +1,4 @@
-using StochasticDiffEq, DiffEqNoiseProcess, BenchmarkTools, Profile, Random
+using StochasticDiffEq, DiffEqNoiseProcess, BenchmarkTools, Profile, Random, SciMLBase
 
 r, σ, t₀, S₀ = 0.02, 0.3, 0.0, 1.0
 T = 1.0
@@ -26,8 +26,6 @@ struct FakeProblem{P<:NoiseProblem, F<:FakeSolution}
     base_prob::P         # shared setup (no duplication)
     solutions::Vector{F}  # precomputed or simulated paths
 end
-
-using SciMLBase
 
 function simple_solve(noise_problem; steps)
     rng = Random.default_rng()
@@ -58,7 +56,7 @@ function generate_ensemble_solution(r, σ, S₀, T, t₀, steps, Npaths)
     end
     return SciMLBase.EnsembleSolution{FakeSolution{Float64, Float64}, 1, FakeProblem}(
         FakeProblem(noise_problem, paths), 1.0, true, nothing)
-    end
+end
 
 Base.length(v::FakeProblem) = Base.length(v.solutions)
 Base.getindex(fp::FakeProblem, i::Int) = fp.solutions[i]
@@ -74,4 +72,4 @@ mean_an = r - σ^2 / 2
 @show mean_an
 @show mean_exp
 
-display(@benchmark generate_ensemble_solution(r, σ, S₀, T,t₀, 1, 1000))
+display(@benchmark generate_ensemble_solution(r, σ, S₀, T,t₀, 1, 5000))
